4fc090b66b6c8c0224397f28a6541c1d4daf5dde
João Paulo Medeiros Rodrigues <jp.estudos17@gmal.com>
2026-01-19 16:47:17 -0300

    Remove Sandbox do versionamento

--- Sandbox/teste.c
+++
@@ -1,9 +0,0 @@
-// Esse código não tem nenhum conteúdo em específico, é somente uma série de
-// testes que eu venho fazendo com o passar do livro
-#include <stdio.h>
-
-int main(void)
-{
-    printf("Hello World\x21\n");
-    return 0;
-}
