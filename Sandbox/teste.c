9c10e596ddcd561f52fb887ff55047e6fc63555f
João Paulo Medeiros Rodrigues <jp.estudos17@gmal.com>
2026-01-19 16:43:54 -0300

    Adciona Sandbox ao gitignore

---
+++ Sandbox/teste.c
@@ -0,0 +1,9 @@
+// Esse código não tem nenhum conteúdo em específico, é somente uma série de
+// testes que eu venho fazendo com o passar do livro
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World\x21\n");
+    return 0;
+}
